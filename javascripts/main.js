1967237bc7caa50f894b861591f1cc868f8e9b78
alan <alan@maozaixuan.localdomain>
2017-01-30 13:19:39 +0000

    change js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,6 @@
-console.log('This would be the main JS file.');
+$(function(){
+	document.write("<script> src=http://lib.sinaapp.com/js/jquery/3.1.0/jquery-3.1.0.min.js </script>");
+	$("button").click(function(){
+		$("p").hide();
+	});
+});
