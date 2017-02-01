b85893b6939318f78741fe920fed418f371e7118
alan <alan@maozaixuan.localdomain>
2017-02-01 17:57:19 +0000

    use jq

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,16 @@
-$(function(){
-	$("button").click(function(){
-		$("p").hide();
+var jq=$.noConflict();
+jq(function(){
+	jq("#dl").hide();
+	jq("button").click(function(){
+		jq("#dl").fadeToggle();
+		jq("#panel").slideToggle("slow");
+		jq("#animate").animate({
+			left:"250px",
+			opacity:"0.5",
+			height:"150px",
+			width:"150px"
+		});
 	});
-});
+		
+})
+
